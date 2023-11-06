c26eabd117689b5749a1dcb335c0561f72ed8ca6
BaegJeonghoon <bb_jhoon@naver.com>
2023-11-06 10:39:56 +0900

    ✨ Navbar 검색 버튼 추가

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,6 +21,25 @@ function Navbar() {
         <div className="navbar-end">
           <ul className="menu menu-horizontal px-1">
             <li>
+                {/* 검색 버튼 */}
+                <button className="btn btn-ghost btn-circle">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    />
+                </svg>
+                </button>
+            </li>
+            <li>
               <Link to="">로그인/회원가입</Link>
             </li>
           </ul>
