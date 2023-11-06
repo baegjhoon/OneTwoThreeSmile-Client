8a0b84da71745392e1f42c8febf86f769f90ea9b
BaegJeonghoon <bb_jhoon@naver.com>
2023-11-06 16:35:59 +0900

    ✨ 배너용 Carousel 컴포넌트 추가 2023. 11. 06 - 16:35

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from "react";
+
+import "../styles/css/components/Carousel.css";
+
+// 임시 이미지 데이터
+const slides = [
+  {
+    eachSlide: "url(https://images.unsplash.com/photo-1620641788421-7a1c342ea42e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80)",
+  },
+  {
+    eachSlide: "url(https://images.unsplash.com/photo-1557682250-33bd709cbe85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2858&q=80)",
+  },
+  {
+    eachSlide: "url(https://images.unsplash.com/photo-1614851099511-773084f6911d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)",
+  },
+  {
+    eachSlide: "url(https://images.unsplash.com/photo-1614849286521-4c58b2f0ff15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80)",
+  },
+  {
+    eachSlide: "url(https://images.unsplash.com/photo-1618556450994-a6a128ef0d9d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1664&q=80)",
+  },
+];
+
+const Carousel = () => {
+  const [active, setActive] = useState(0);
+  const [autoplay, setAutoplay] = useState(true);
+  const max = slides.length;
+
+  const intervalBetweenSlides = () =>
+    autoplay && setActive(active === max - 1 ? 0 : active + 1);
+
+  useEffect(() => {
+    const interval = setInterval(() => intervalBetweenSlides(), 3000);
+    return () => clearInterval(interval);
+  });
+
+  const toggleAutoPlay = () => setAutoplay(!autoplay);
+
+  const nextOne = () => setActive(active === max - 1 ? 0 : active + 1);
+
+  const prevOne = () => active > 0 && setActive(active - 1);
+
+  const isActive = (value) => active === value && "active";
+
+  const setSliderStyles = () => {
+    const transition = active * -100;
+
+    return {
+      width: slides.length * 100 + "vw",
+      transform: "translateX(" + transition + "vw)",
+    };
+  };
+
+  const renderSlides = () =>
+    slides.map((item, index) => (
+      <div
+      className={`each-slide ${isActive(index)}`}
+      key={index}
+      style={{ backgroundImage: item.eachSlide }}
+    >
+      <h1>하나 둘 셋, 스마일 ˃ ⩌˂</h1>
+    </div>
+    ));
+
+  const renderDots = () =>
+    slides.map((slide, index) => (
+      <li className={isActive(index) + " dots"} key={index}>
+        <button onClick={() => setActive(index)}>
+          <span>&#9679;</span>
+        </button>
+      </li>
+    ));
+
+  const renderPlayStop = () =>
+    autoplay ? (
+      <svg fill="#FFFFFF" height="24" viewBox="0 0 24 24" width="24">
+        <path d="M0 0h24v24H0z" fill="none" />
+        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 14H9V8h2v8zm4 0h-2V8h2v8z" />
+      </svg>
+    ) : (
+      <svg fill="#FFFFFF" height="24" viewBox="0 0 24 24" width="24">
+        <path d="M0 0h24v24H0z" fill="none" />
+        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z" />
+      </svg>
+    );
+
+  const renderArrows = () => (
+    <>
+      <button type="button" className="arrows prev" onClick={() => prevOne()}>
+        <svg fill="#FFFFFF" width="50" height="50" viewBox="0 0 24 24">
+          <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
+          <path d="M0 0h24v24H0z" fill="none" />
+        </svg>
+      </button>
+      <button type="button" className="arrows next" onClick={() => nextOne()}>
+        <svg fill="#FFFFFF" height="50" viewBox="0 0 24 24" width="50">
+          <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
+          <path d="M0 0h24v24H0z" fill="none" />
+        </svg>
+      </button>
+    </>
+  );
+
+  return (
+    <section className="slider">
+      <div className="wrapper" style={setSliderStyles()}>
+        {renderSlides()}
+      </div>
+      {renderArrows()}
+      <ul className="dots-container">{renderDots()}</ul>
+      <button type="button" className="toggle-play" onClick={toggleAutoPlay}>
+        {renderPlayStop()}
+      </button>
+    </section>
+  );
+};
+
+export default Carousel;
