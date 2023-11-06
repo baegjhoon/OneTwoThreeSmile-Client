3719df683d0473a7402cc61d19bf9782ab388edc
BaegJeonghoon <bb_jhoon@naver.com>
2023-11-06 15:48:38 +0900

    ✨ 검색 컴포넌트 분리 및 애니메이션 적용

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Search from "./atoms/button/Search"
+
 function Navbar() {
   return (
     <React.Fragment>
@@ -22,22 +24,7 @@ function Navbar() {
           <ul className="menu menu-horizontal px-1">
             <li>
                 {/* 검색 버튼 */}
-                <button className="btn btn-ghost btn-circle">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    />
-                </svg>
-                </button>
+                <Search />
             </li>
             <li>
               <Link to="">로그인/회원가입</Link>
---
+++ src/components/atoms/button/Search.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import "../../../styles/css/components/atoms/button/Search.css";
+
+function Search() {
+  return (
+    <>
+      <form className="search" action="" autoComplete="on">
+        <input
+          id="search"
+          name="search"
+          type="text"
+          placeholder="What're we looking for ?"
+        />
+        <button className="btn btn-ghost btn-circle" type="submit">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default Search;
