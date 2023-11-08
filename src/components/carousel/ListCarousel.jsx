ff69b072c45819816b06333e59817e28bfaac10b
BaegJeonghoon <bb_jhoon@naver.com>
2023-11-08 10:58:46 +0900

    ✨ 이미지(상품, ...) 리스트가 보여질 페이징 캐러셀 구현 - #1

---
+++ src/components/carousel/ListCarousel.jsx
@@ -0,0 +1,45 @@
+import React, { useRef, useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import 'swiper/css/navigation';
+
+import "../../styles/css/components/carousel/ListCarousel.css";
+
+function ListCarousel() {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  };
+
+  return (
+    <>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        loop={true}
+        navigation={true}
+        pagination={pagination}
+        modules={[Pagination, Navigation]}
+        className="mySwiper swiper-h"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
+
+export default ListCarousel;
